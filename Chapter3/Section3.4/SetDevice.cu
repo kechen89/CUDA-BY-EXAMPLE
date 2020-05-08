7e8c65caff872a69064b71be39578b64363116d7
Chen <wg-kechen@compute1-exec-205.ris.wustl.edu>
2020-05-08 00:02:04 +0000

    Section 3.4 cudaSetDevice

---
+++ Chapter3/Section3.4/SetDevice.cu
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main(void) {
+    cudaDeviceProp prop;
+    int dev;
+
+    cudaGetDevice( &dev );
+    std::cout << "ID of current CUDA device: " << dev << std::endl;
+
+    memset( &prop, 0, sizeof( cudaDeviceProp ) );
+    prop.major = 1;
+    prop.minor = 3;
+    cudaChooseDevice( &dev, &prop );
+    std::cout << "ID of CUDA device closest to revision 1.3: " << dev << std::endl;
+    cudaSetDevice( dev );
+
+    return 0;
+}
